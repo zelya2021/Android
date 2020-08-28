7637333e40a57e9407cbe974b241974f89becdd7
zelya2021 <zelenckina1999@gmail.com>
2020-08-28 12:43:56 +0300

    created add person activity

--- PeopleCrud/app/src/main/java/com/example/peoplecrud/MainActivity.java
+++ PeopleCrud/app/src/main/java/com/example/peoplecrud/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.peoplecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Contacts;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.peoplecrud.adapters.PeopleAdapter;
@@ -13,6 +16,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView lvPeople;
+    Button btnAddNewPerson;
     ArrayList<Person> people = new ArrayList<>();
     {
         for (int i = 1; i <= 10; i++) {
@@ -26,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
         lvPeople = findViewById(R.id.lvPeople);
         PeopleAdapter peopleAdapter = new PeopleAdapter(this, R.layout.people_layout, people);
         lvPeople.setAdapter(peopleAdapter);
+        btnAddNewPerson=findViewById(R.id.btnAddNewPerson);
+
+        btnAddNewPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddPerson.class);
+                //запуск нового Activity
+                startActivity(intent);
+            }
+        });
     }
 }
